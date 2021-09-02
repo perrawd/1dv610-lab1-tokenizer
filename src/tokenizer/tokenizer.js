9570e04dec2a3bd2caa12e5544deddaa50893c0f
Per Rawdin <pr222iw@student.lnu.se>
2021-09-02 22:01:53 +0200

    Add tokenize method and tests

--- src/tokenizer/tokenizer.js
+++ src/tokenizer/tokenizer.js
@@ -14,4 +14,63 @@ export default class Tokenizer {
     this.type = type
     this.string = string
   }
+
+  /**
+   * Tokenizes a string.
+   *
+   * @returns {Array} An Array of objects for the tokenized string.
+   */
+  tokenize () {
+    let regex
+    const tokenizedString = []
+
+    switch (this.type) {
+      case 'WordAndDotGrammar':
+        regex = /([.\s])/
+    }
+
+    // TODO: If possible, fix/finalize regex in order to remove filter.
+    const splittedString = this.string
+      .split(regex)
+      .filter(w => w !== '' && w !== ' ')
+
+    splittedString.forEach((subString, index) => {
+      const tokenType = this._determineTokenType(subString)
+
+      tokenizedString.push({
+        tokenMatch: index,
+        tokenType: tokenType,
+        value: subString
+      })
+    })
+
+    return tokenizedString
+  }
+
+  /**
+   * Determines the token-type of a string.
+   *
+   * @param {any} subString The sub-string to determine type for.
+   * @returns {string} Type of the sub-string.
+   */
+  _determineTokenType (subString) {
+    let tokenType
+
+    if (subString === '.') {
+      tokenType = 'DOT'
+    } else if (subString.length > 0) {
+      tokenType = 'WORD'
+    }
+
+    return tokenType
+  }
+
+  /**
+   * Prints a table of the tokenized string.
+   *
+   * @param {object} input The tokenized string.
+   */
+  printTable (input) {
+
+  }
 }
--- src/tokenizer/tokenizer.test.js
+++ src/tokenizer/tokenizer.test.js
@@ -3,14 +3,26 @@ import Tokenizer from './tokenizer'
 
 const wordDotGrammar = new Tokenizer('WordAndDotGrammar', 'Meningen består av ord.')
 
-test('test word and dot grammar', () => {
-  expect(wordDotGrammar.tokenize()).toStrictEqual(
-    [
-      { tokenMatch: 0, tokenType: 'WORD', value: 'Meningen' },
-      { tokenMatch: 1, tokenType: 'WORD', value: 'består' },
-      { tokenMatch: 2, tokenType: 'WORD', value: 'av' },
-      { tokenMatch: 3, tokenType: 'WORD', value: 'ord' },
-      { tokenMatch: 4, tokenType: 'DOT', value: '.' }
-    ]
-  )
+const wordDotGrammarOutput = [
+  { tokenMatch: 0, tokenType: 'WORD', value: 'Meningen' },
+  { tokenMatch: 1, tokenType: 'WORD', value: 'består' },
+  { tokenMatch: 2, tokenType: 'WORD', value: 'av' },
+  { tokenMatch: 3, tokenType: 'WORD', value: 'ord' },
+  { tokenMatch: 4, tokenType: 'DOT', value: '.' }
+]
+
+test('test toMatchObject word and dot grammar', () => {
+  expect(wordDotGrammar.tokenize()).toMatchObject(wordDotGrammarOutput)
+})
+
+test('test toStrictEqual word and dot grammar', () => {
+  expect(wordDotGrammar.tokenize()).toStrictEqual(wordDotGrammarOutput)
+})
+
+test('test determineTokenType method (WORD)', () => {
+  expect(wordDotGrammar._determineTokenType('Ord')).toBe('WORD')
+})
+
+test('test determineTokenType method (DOT)', () => {
+  expect(wordDotGrammar._determineTokenType('.')).toBe('DOT')
 })
