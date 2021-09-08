bd9454c1068c2f0d1d329d8b25fbe9ba1f344471
Per Rawdin <pr222iw@student.lnu.se>
2021-09-08 22:31:50 +0200

    Add Enums for Grammar

---
+++ src/tokenizer/grammar.js
@@ -0,0 +1,20 @@
+/**
+ * Module for Grammar.
+ *
+ * @module src/tokenizer/grammar
+ * @author Per Rawdin <per.rawdin@student.lnu.se>
+ * @version 1.0.0
+ */
+
+/**
+ * Enum for Grammar.
+ *
+ * @readonly
+ * @enum {string}
+ */
+const GRAMMAR = Object.freeze({
+  WORD_AND_DOT: 'WordAndDotGrammar',
+  ARITHMETIC: 'ArithmeticGrammar'
+})
+
+export default GRAMMAR
