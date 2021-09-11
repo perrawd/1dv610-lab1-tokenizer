458c5e1a128f17136989736b36db0646383903cc
Per Rawdin <pr222iw@student.lnu.se>
2021-09-11 21:23:17 +0200

    Refactor Tokenizer and tokenize() method

--- src/tokenizer/tokenizer.js
+++ src/tokenizer/tokenizer.js
@@ -1,31 +1,27 @@
 import TokenizedSubString from './tokenizedSubString.js'
-
 /**
- * Creates a tokenizer.
+ *
  *
  * @class Tokenizer
  */
 export default class Tokenizer {
   /**
-   * Constructor.
+   * Creates an instance of Tokenizer.
    *
-   * @param {string} type The type of tokenization.
-   * @param {string} string The string to be tokenized.
+   * @param {*} type The Grammatic type.
+   * @param {*} string The string to be tokenized.
+   * @memberof Tokenizer
    */
   constructor (type, string) {
-    this.type = type
+    Object.assign(this, type)
     this.string = string
-
-    this.lexicalGrammar = this.tokenize()
-
-    this.currentIndex = 0
-    this.currentToken = this.lexicalGrammar[this.currentIndex]
   }
 
   /**
    * Tokenizes a string.
    *
-   * @returns {Array} An Array of objects for the tokenized string.
+   * @returns {Array} array
+   * @memberof Tokenizer
    */
   tokenize () {
     try {
@@ -33,115 +29,29 @@ export default class Tokenizer {
         throw new Error('Empty string provided. Please provide a valid string.')
       }
 
-      let regex
-      const tokenizedString = []
-
-      switch (this.type) {
-        case 'WordAndDotGrammar':
-          regex = /([.\s])/
-          break
-        case 'ArithmeticGrammar':
-          regex = /(?<=[-+*/])|(?=[-+*/])/
-          break
-        default:
-          throw new Error('Missing valid lexical grammatic type.')
-      }
-
-      // TODO: If possible, fix/finalize regex in order to remove filter.
       const splittedString = this.string
-        .split(regex)
+        .split(new RegExp(this.splitPattern))
         .filter(token => token !== '' && token !== ' ')
 
-      splittedString.forEach((subString, index) => {
-        const tokenType = this._determineTokenType(this.type, subString)
-        const { token } = new TokenizedSubString(index, tokenType, subString)
+      const tokenizedString = []
 
-        tokenizedString.push(token)
+      splittedString.forEach((subString, index) => {
+        for (const property in this.grammarTypes) {
+          if (new RegExp(this.grammarTypes[property]).test(subString)) {
+            const { token } = new TokenizedSubString(
+              index,
+              property,
+              subString
+            )
+            tokenizedString.push(token)
+            break
+          }
+        }
       })
 
       return tokenizedString
     } catch (error) {
       console.error(error)
     }
-  }
-
-  /**
-   * Move current to next.
-   *
-   * @param {string} direction The direction to move the pointer of the current token to.
-   *
-   */
-  moveCurrentTokenTo (direction) {
-    try {
-      switch (direction) {
-        case 'previous':
-          if ((this.currentIndex - 1) < 0) {
-            throw new Error('First index reached')
-          }
-          this.currentIndex -= 1
-          this.currentToken = this.lexicalGrammar[this.currentIndex]
-          break
-        case 'next':
-          if ((this.currentIndex + 1) > (this.lexicalGrammar.length - 1)) {
-            throw new Error('Last index reached')
-          }
-          this.currentIndex += 1
-          this.currentToken = this.lexicalGrammar[this.currentIndex]
-          break
-        default:
-          throw new Error('Invalid direction')
-      }
-    } catch (error) {
-      console.error(error.message)
-    }
-  }
-
-  /**
-   * Determines the token-type of a string.
-   *
-   * @param {string} type The sub-string to determine type for.
-   * @param {string} subString The sub-string to determine type for.
-   * @returns {string} Type of the sub-string.
-   */
-  _determineTokenType (type, subString) {
-    try {
-      let tokenType
-
-      // TODO: Look into refactoring this.
-      switch (type) {
-        case 'WordAndDotGrammar':
-          if (subString === '.') {
-            tokenType = 'DOT'
-          } else if (subString.length > 0) {
-            tokenType = 'WORD'
-          }
-          break
-        case 'ArithmeticGrammar':
-          switch (subString) {
-            case '+':
-              tokenType = 'ADD'
-              break
-            case '-':
-              tokenType = 'SUB'
-              break
-            default:
-              tokenType = 'NUMBER'
-          }
-          break
-      }
-
-      return tokenType
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  /**
-   * Prints a table of the tokenized string.
-   *
-   * @param {object} input The tokenized string.
-   */
-  printTable (input) {
-
   }
 }
---
+++ src/tokenizer/tokenizer.old.js
@@ -0,0 +1,147 @@
+import TokenizedSubString from './tokenizedSubString.js'
+
+/**
+ * Creates a tokenizer.
+ *
+ * @class Tokenizer
+ */
+export default class Tokenizer {
+  /**
+   * Constructor.
+   *
+   * @param {string} type The type of tokenization.
+   * @param {string} string The string to be tokenized.
+   */
+  constructor (type, string) {
+    this.type = type
+    this.string = string
+
+    this.lexicalGrammar = this.tokenize()
+
+    this.currentIndex = 0
+    this.currentToken = this.lexicalGrammar[this.currentIndex]
+  }
+
+  /**
+   * Tokenizes a string.
+   *
+   * @returns {Array} An Array of objects for the tokenized string.
+   */
+  tokenize () {
+    try {
+      if (!this.string.length) {
+        throw new Error('Empty string provided. Please provide a valid string.')
+      }
+
+      let regex
+      const tokenizedString = []
+
+      switch (this.type) {
+        case 'WordAndDotGrammar':
+          regex = /([.\s])/
+          break
+        case 'ArithmeticGrammar':
+          regex = /(?<=[-+*/])|(?=[-+*/])/
+          break
+        default:
+          throw new Error('Missing valid lexical grammatic type.')
+      }
+
+      // TODO: If possible, fix/finalize regex in order to remove filter.
+      const splittedString = this.string
+        .split(regex)
+        .filter(token => token !== '' && token !== ' ')
+
+      splittedString.forEach((subString, index) => {
+        const tokenType = this._determineTokenType(this.type, subString)
+        const { token } = new TokenizedSubString(index, tokenType, subString)
+
+        tokenizedString.push(token)
+      })
+
+      return tokenizedString
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  /**
+   * Move current to next.
+   *
+   * @param {string} direction The direction to move the pointer of the current token to.
+   *
+   */
+  moveCurrentTokenTo (direction) {
+    try {
+      switch (direction) {
+        case 'previous':
+          if ((this.currentIndex - 1) < 0) {
+            throw new Error('First index reached')
+          }
+          this.currentIndex -= 1
+          this.currentToken = this.lexicalGrammar[this.currentIndex]
+          break
+        case 'next':
+          if ((this.currentIndex + 1) > (this.lexicalGrammar.length - 1)) {
+            throw new Error('Last index reached')
+          }
+          this.currentIndex += 1
+          this.currentToken = this.lexicalGrammar[this.currentIndex]
+          break
+        default:
+          throw new Error('Invalid direction')
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  /**
+   * Determines the token-type of a string.
+   *
+   * @param {string} type The sub-string to determine type for.
+   * @param {string} subString The sub-string to determine type for.
+   * @returns {string} Type of the sub-string.
+   */
+  _determineTokenType (type, subString) {
+    try {
+      let tokenType
+
+      // TODO: Look into refactoring this.
+      switch (type) {
+        case 'WordAndDotGrammar':
+          if (subString === '.') {
+            tokenType = 'DOT'
+          } else if (subString.length > 0) {
+            tokenType = 'WORD'
+          }
+          break
+        case 'ArithmeticGrammar':
+          switch (subString) {
+            case '+':
+              tokenType = 'ADD'
+              break
+            case '-':
+              tokenType = 'SUB'
+              break
+            default:
+              tokenType = 'NUMBER'
+          }
+          break
+      }
+
+      return tokenType
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  /**
+   * Prints a table of the tokenized string.
+   *
+   * @param {object} input The tokenized string.
+   */
+  printTable (input) {
+
+  }
+}
