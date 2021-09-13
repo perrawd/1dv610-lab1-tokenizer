ac16f7e1472a5733cf15f9c4770792bd2702ac98
Per Rawdin <pr222iw@student.lnu.se>
2021-09-13 16:42:05 +0200

    Add moveActiveTokenTo() in Tokenizer class

--- src/tokenizer/tokenizer.js
+++ src/tokenizer/tokenizer.js
@@ -62,4 +62,34 @@ export default class Tokenizer {
       console.error(error)
     }
   }
+
+  /**
+   * Move current to next.
+   *
+   * @param {string} direction The direction to move the pointer of the current token to.
+   *
+   */
+  moveActiveTokenTo (direction) {
+    try {
+      switch (direction) {
+        case 'previous':
+          if ((this.activeTokenIndex - 1) < 0) {
+            throw new Error('First index reached')
+          }
+          this.activeTokenIndex -= 1
+          break
+        case 'next':
+          if ((this.activeTokenIndex + 1) > (this.lexicalGrammar.length - 1)) {
+            throw new Error('Last index reached')
+          }
+          this.activeTokenIndex += 1
+          break
+        default:
+          throw new Error('Invalid direction')
+      }
+      this.activeToken = this.lexicalGrammar[this.activeTokenIndex]
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 }
