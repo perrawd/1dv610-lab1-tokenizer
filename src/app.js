01081ad72abda6c11b6011dd5438e37868cc8ebd
Per Rawdin <pr222iw@student.lnu.se>
2021-10-17 11:17:45 +0200

    Update exports in main module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 import LexicalAnalysis from "./lexical-analysis.js";
 import LexicalGrammar from "./lib/lexical-grammar/lexical-grammar";
 import GRAMMAR_NAME from "./lib/lexical-grammar/grammar-name.js";
+
+export { LexicalAnalysis, LexicalGrammar, GRAMMAR_NAME }
