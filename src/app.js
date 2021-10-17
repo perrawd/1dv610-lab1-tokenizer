67cf5a280134e5beedd13e25a7394eb9e34391db
Per Rawdin <pr222iw@student.lnu.se>
2021-10-17 11:11:27 +0200

    Update main imports in app.js

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import LexicalAnalysis from "./lexical-analysis.js";
+import LexicalGrammar from "./lib/lexical-grammar/lexical-grammar";
+import GRAMMAR_NAME from "./lib/lexical-grammar/grammar-name.js";
