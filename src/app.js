2b12bd8e799c3849fb073ba031a90bdb5884e73b
Per Rawdin <pr222iw@student.lnu.se>
2021-10-17 11:22:19 +0200

    Add Token in export main

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import LexicalAnalysis from "./lexical-analysis.js";
-import LexicalGrammar from "./lib/lexical-grammar/lexical-grammar";
+import LexicalGrammar from "./lib/lexical-grammar/lexical-grammar.js";
 import GRAMMAR_NAME from "./lib/lexical-grammar/grammar-name.js";
+import Token from './lib/token/token.js'
 
-export { LexicalAnalysis, LexicalGrammar, GRAMMAR_NAME }
+export { LexicalAnalysis, LexicalGrammar, GRAMMAR_NAME, Token }
